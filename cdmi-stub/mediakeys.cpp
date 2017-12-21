b11c1342463c50bc149e745c0822eba39c16530a
Sivasubramanian <sivasubramanian.patchaiperumal@linaro.org>
2017-12-21 21:45:11 +0530

    Revert ClearKey fix

--- cdmi-stub/mediakeys.cpp
+++ cdmi-stub/mediakeys.cpp
@@ -33,11 +33,11 @@ CMediaKeys::CMediaKeys() {}
 CMediaKeys::~CMediaKeys(void) {}
 
 bool CMediaKeys::IsTypeSupported(
-    const char *f_pwszKeySystem,
-    const char *f_pwszMimeType) const {
+    const char *f_pwszMimeType,
+    const char *f_pwszKeySystem) const {
   bool isSupported = false;
   if (f_pwszKeySystem) 
-      if (strcmp(f_pwszKeySystem, "org.w3.clearkey") == 0)
+      if (strcmp(f_pwszKeySystem, "org.w3.clearkey") == 0 || strcmp(f_pwszKeySystem, "org.chromium.externalclearkey") == 0)
           isSupported = true;
 
   return isSupported;
